2ebb8afd641927ff7e2335268d44846a2ac2f4ea
HaydenKG <HaydenK82@gmail.com>
2021-12-08 22:28:03 -0500

    Enhancing breathing system
    Starts when button clicked.
    In sync with css animation

--- Subpages/Breathing.js
+++ Subpages/Breathing.js
@@ -2,14 +2,27 @@ var displaycounter = document.getElementById("boxbreathing_counter");
 let counter = 0;
 let cycle = 0;
 let rounds = 0;
+var breathinginterval;
 
-setInterval(breathingcycle, 1000);
+//TODO: smooth scroll maybe with get y pos of element and than scroll to it by..
+function start() {
+        const yOffset = -200; 
+        const y = displaycounter.getBoundingClientRect().top + yOffset;
+        window.scrollTo({ top: y, behavior: 'smooth' });
+        breathingcycle();
+        breathinginterval = setInterval(breathingcycle, 1000);
+        document.getElementById('shape').className = "shape";
+}
 
+//TODO: check if really 3 rounds are over
 function breathingcycle() {
-        console.log(rounds);    
-        if (rounds >= 3) {
+        ++cycle;
+
+        if (rounds >= 3) { 
                 displaycounter.innerHTML = "Finished 3 rounds";
-                clearInterval(breathingcycle);
+                clearInterval(breathinginterval);
+                document.getElementById('shape').className = "";
+                return;
         }
 
         if (cycle == 16) {
@@ -17,17 +30,13 @@ function breathingcycle() {
                 rounds++;
         }
         
-        if (cycle < 4) {
-                counter++;
-                displaycounter.innerHTML = "In for <br /> &nbsp; &nbsp;" + counter;
+        else if (cycle <= 4) {
+                displaycounter.innerHTML = "In for <br /> &nbsp; &nbsp;" + ++counter;
         }
-        
-        if (cycle >= 4 && cycle < 9 || cycle > 11 && cycle < 15)
+        else if (cycle > 4 && cycle <= 8 || cycle > 12 && cycle < 15)
                 displaycounter.innerHTML = "hold it";   
         
-        if (cycle > 7 && cycle < 12) {
-                displaycounter.innerHTML = "Out for <br /> &nbsp; &ensp;" + counter;
-                counter--;
+        else if (cycle > 8 && cycle < 13) {
+                displaycounter.innerHTML = "Out for <br /> &nbsp; &ensp;" + counter--;
         }
-        cycle++;
 }
