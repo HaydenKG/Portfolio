84c62687cd90bfad391a49ad455792f393ae20a2
HaydenKG <HaydenK82@gmail.com>
2022-01-04 10:46:21 -0500

    added photography site
    Site is now available on main page after clicking on the more button

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const furtherprojbtn = document.querySelector('#furtherprojbtn')
+const furtherprojectsexpand = document.querySelector('.furtherprojectshidden')
+let clicked = false;
 
-menu.addEventListener('click', function(){
+menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+})
+
+furtherprojbtn.addEventListener('click', function () {
+    furtherprojectsexpand.classList.toggle('active');
+    if (clicked) {
+        furtherprojbtn.innerHTML = "more";
+        clicked = false;
+    } else {
+        furtherprojbtn.innerHTML = "hide";
+        clicked = true;
+    }
 })
